f8f24645bb8548f7b0e766eef5716e493bc5ddb0
holmanl <hlan@magnitude.com>
2014-02-20 22:27:41 -0800

    [OracleODBC][ORLC-26]SQLTables API call may fail.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.0/Product/": change = 96487]

--- Source/DataEngine/Metadata/OCITablesMetadataSource.cpp
+++ Source/DataEngine/Metadata/OCITablesMetadataSource.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:470139027f635fc28fd2323732bd77bd9691fcc0b42c3191b77569286c33513a
-size 5236
+oid sha256:a5592838cd9e34c4d5864da1360f9ef2fcdf1e357f48a81335e8e7daf1f2afa4
+size 8792
