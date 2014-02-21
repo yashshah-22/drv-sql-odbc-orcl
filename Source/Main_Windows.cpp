7d2fb8f2d2cb188f63ee5c69fdd96ea2ee89b07b
holmanl <hlan@magnitude.com>
2014-02-20 17:35:35 -0800

    [OracleODBC M1.1]Fixed issue where an extern veriable is declared static, causing the DSN config dialog not to pop up.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.0/Product/": change = 96465]

--- Source/Main_Windows.cpp
+++ Source/Main_Windows.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea391b99c8d76079e7db7a3d19f606bfa01ae905bd3243ce6b9544a3cff9d807
-size 2258
+oid sha256:6c88279b19c7c99af027fa0db3115e5ecaaf52839e14fcabf90279ded0070333
+size 2251
--- Source/resource.h
+++ Source/resource.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15b600ded8efbf1547941cbada80ae3d4daec58417f7e544ff6b25ed8b53cedc
-size 1456
+oid sha256:de572de530d67281f1c2e95734034948b31643d9f59ab16303fe126bcf22f8eb
+size 1413
