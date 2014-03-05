9b499eee0bb696fc0bb5528fbceef1878d61f692
holmanl <hlan@magnitude.com>
2014-03-04 21:44:37 -0800

    [OracleODBC M1.0]Update version number to 1.0.2.0003. Corrected driver and product names to Simba Oracle ODBC Driver.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.0/Product/": change = 97299]

--- Source/Core/OCIDriver.cpp
+++ Source/Core/OCIDriver.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d3a29409b07d4c5db1085e824c23e230f40f280d8c823ce4741ba2fe9815176
-size 3394
+oid sha256:276dd3ca826df3b8db3544ec1aba20d101f5bf33d0249b9ea2c150f89e70321a
+size 3399
