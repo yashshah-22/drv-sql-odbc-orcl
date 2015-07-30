3a3989efb7348cbc74dd653ad633ddee42578847
deviny <a@b>
2015-07-30 13:42:13 -0800

    [Oracle ODBC] refactor build script
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.0/Product/": change = 135171]

--- Bamboo/Scripts/addDriverContent.pl
+++ Bamboo/Scripts/addDriverContent.pl
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80318c779d11df33e890f1bcc3df23da7aa1e1b5f9623187364a8d2974972038
-size 1003
+oid sha256:c0a679da33087627cf41d6971ecfa2ba974b338b7cf6ff9a74cc8a81539010ae
+size 1369
