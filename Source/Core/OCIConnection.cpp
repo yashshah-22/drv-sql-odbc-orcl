e0bc69ff8e33438cc6d44f5297e1be920e5063f9
harryx <a@b>
2016-07-27 08:57:00 -0800

    [ORCL-114][OEM][GA][SEN 10.0][LOW] - 1.1.0 Bug fix: Data Conversion failure when BindParameter before SQLPrepare. Turn off support for SQL_ATTRE_AUTO_IPD for now, because we never support it before.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 184918]

--- Source/Core/OCIConnection.cpp
+++ Source/Core/OCIConnection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2805eb55b9f69fc2216f3ee1061b1c3c2e0e2fc5c35403c53527230d633afd61
-size 41299
+oid sha256:6bded29dbffbfe3425525c57c8c8a31c63d1c75b73bc18c27e786b7402667ba7
+size 41300
