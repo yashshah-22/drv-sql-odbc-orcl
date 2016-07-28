21e90cf6d4ba7105ad0b1ef7365a62ca5b538a5a
Dicky Lau <dicky.lau@insightsoftware.com>
2016-07-28 09:19:16 -0800

    [OracleODBC][ORCL-132] Fix NCLOB can't display correct result and return sql error
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 185148]

--- Source/DataEngine/Buffers/OCICLOBBuffer.cpp
+++ Source/DataEngine/Buffers/OCICLOBBuffer.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7856e2f78fdd9e773ccb0860e32c3bc831a056795e815a6d0b0a85c452a49a8b
-size 7296
+oid sha256:1995f406a1856f62cd9ab8815fb3b6b972b25a013432279a459cd4d4b688960b
+size 7586
