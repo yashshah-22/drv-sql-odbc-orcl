ab251eadb955b39eb269eff3be8bafab874bd9f4
Dicky Lau <dicky.lau@insightsoftware.com>
2016-07-28 09:18:59 -0800

    [OracleODBC][ORCL-92]Fix numeric data issue
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 185146]

--- Source/CustomDataTypes/DataTypes/OCICustomNumeric.cpp
+++ Source/CustomDataTypes/DataTypes/OCICustomNumeric.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15171555fbf86499f67bd2f174c5af64d982fe12ec0610a13c1199060e7f93b3
-size 8379
+oid sha256:9043b0e40d1b16ef46c3dd351926faa5fd5c0d3efb40f633191953acd9e9c705
+size 9190
