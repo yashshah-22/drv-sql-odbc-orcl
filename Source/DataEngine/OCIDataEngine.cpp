61330db7f2460b325af9aeaec0c3683d8aba6abb
Dicky Lau <dicky.lau@insightsoftware.com>
2016-07-28 15:29:04 -0800

    [OracleODBC][ORCL-140] Fix sql test data type Real and Float expected SQL_FLOAT but got SQL_DOUBLE
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 185279]

--- Source/DataEngine/OCIDataEngine.cpp
+++ Source/DataEngine/OCIDataEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:983d99ff58adcacc085747fe33d02e97d6f09af22a2fd1d683170f35224e86d6
-size 46029
+oid sha256:5e4830a6ceb5c863bc608e3bb92aa97a3f5b7352e3cc7010e8f4910663c61fe6
+size 46028
