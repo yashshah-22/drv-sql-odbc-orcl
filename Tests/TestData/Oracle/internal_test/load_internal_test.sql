625f39e2015eeb72da33cba9a8ef48608adb2f77
tiffanyl <a@b>
2016-07-28 15:05:26 -0800

    [ORCL][ORCL-84] Create Data Conversion tests
    - Added TestSuite file
    - Added TestSets
    - Added test data under schema SEN
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 185270]

--- Tests/TestData/Oracle/internal_test/load_internal_test.sql
+++ Tests/TestData/Oracle/internal_test/load_internal_test.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb73bb8281e7455a6a890817026adf14e23c6b847f09ecd588dcbdc5a220737e
-size 209704
+oid sha256:4bbee7ac83a33682187536a383533c2659b6ee864c0a735a6de93e1c40b9e850
+size 210174
---
+++ Tests/TestData/Oracle/sen_test/load_sen_test.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c6161cf3f571b7777e292a91c5a9dcf042548d59fc3f83073f6961e092dd3c63
+size 168222
