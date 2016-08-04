2d4a1739435bbc671648eba5a3448c8c5bc83a64
tiffanyl <a@b>
2016-08-04 13:54:59 -0800

    [ORCL][ORCL-149][ORCL-150] Fix WChar test data
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 186011]

--- Tests/TestData/Oracle/sen_test/load_sen_test.sql
+++ Tests/TestData/Oracle/sen_test/load_sen_test.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6161cf3f571b7777e292a91c5a9dcf042548d59fc3f83073f6961e092dd3c63
+oid sha256:8d3596ce2a217ea620c32d8c8cfbe471440a4f8e1ad8c51685c53ec0022e5884
 size 168222
