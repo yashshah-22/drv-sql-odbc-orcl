cab5276ecaab6d23238fcabb2c9aea8ec31cc32c
Dicky Lau <dicky.lau@insightsoftware.com>
2016-08-05 15:01:43 -0800

    [OracleODBC][ORCL-138] fix C_TO_SQL_FLOAT crash
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 186201]

--- Source/CustomDataTypes/DataTypes/OCICustomNumeric.cpp
+++ Source/CustomDataTypes/DataTypes/OCICustomNumeric.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9043b0e40d1b16ef46c3dd351926faa5fd5c0d3efb40f633191953acd9e9c705
-size 9190
+oid sha256:10f5ca7ff9d45c9a1a180dac4a2d28ecd422115b43dfa9511d81db2f03ac8a22
+size 11752
