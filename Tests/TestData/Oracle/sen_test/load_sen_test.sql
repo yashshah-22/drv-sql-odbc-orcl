16e277dfc58b5ae470bb8fea1746551c7a45c8de
tiffanyl <a@b>
2016-08-08 15:10:41 -0800

    [ORCL][ORCL-130] Modify TestSuite file
    - Ignore faulty test cases 
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 186455]

--- Tests/TestData/Oracle/sen_test/load_sen_test.sql
+++ Tests/TestData/Oracle/sen_test/load_sen_test.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d3596ce2a217ea620c32d8c8cfbe471440a4f8e1ad8c51685c53ec0022e5884
-size 168222
+oid sha256:4c2ee1d91ac6020e62835ecab92177afed1b650753cb30822dd4cffa11e16875
+size 166090
