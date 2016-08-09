22b8dee9819560db1bc16619226abfca5e6e6cba
tiffanyl <a@b>
2016-08-09 14:34:34 -0800

    [ORCL] Modify ini file format
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 186702]

--- Bamboo/Scripts/NV_modules/MakeDarwinIniFiles.pm
+++ Bamboo/Scripts/NV_modules/MakeDarwinIniFiles.pm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16a31cfa1bbc6f76cc22cb04d37c2344285a7df7c94490455b599655a03806ee
-size 3641
+oid sha256:323a9ae6c464d0e63fef05067a0abe217b8830bc634ac6e1a6866ec70617b458
+size 3340
--- Bamboo/Scripts/NV_modules/MakeLinuxIniFiles.pm
+++ Bamboo/Scripts/NV_modules/MakeLinuxIniFiles.pm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4190e0c757ff5441f2fd4ac4bdc3ff90cdb35fee62e50d56d1e1ac142885a451
-size 6439
+oid sha256:478582c79589ab299702cee2764dd9dfa0256add93f804b4b77699040a857296
+size 5856
