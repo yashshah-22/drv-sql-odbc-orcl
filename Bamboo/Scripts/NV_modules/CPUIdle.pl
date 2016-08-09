bbc04c1fc41d5333d643e7c7e903ff116615a332
tiffanyl <a@b>
2016-08-09 08:54:49 -0800

    [ORCL] Update Bamboo scripts
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 186550]

--- Bamboo/Scripts/NV_modules/CPUIdle.pl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0171714ed14f51cd6e7d09871deb1c11ad961e27a3a3210e14487b9f638e5cf8
-size 4302
---
+++ Bamboo/Scripts/NV_modules/SetWindowsRegistryForTest.pm
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a936481c517154053030300c139a58f6a8bfe9fd673575004234df127e5b7b26
+size 4267
