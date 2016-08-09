97ca866415998686cdb95ecbd0482bff16beb85f
tiffanyl <a@b>
2016-08-09 15:28:48 -0800

    [ORCL] Remove simba branding from make ini files
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 186736]

--- Bamboo/Scripts/NV_modules/MakeDarwinIniFiles.pm
+++ Bamboo/Scripts/NV_modules/MakeDarwinIniFiles.pm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eecfcd950827f16e0b94e7627b2b4d0d31b1e4b68ad70cf0c6b84ce7919df777
-size 3340
+oid sha256:c38f8194296808ae69845e298cd95860df063e24d1687b962c49b78e81bac96e
+size 3316
