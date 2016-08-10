ecf4203e64908d5b09ca90990d09dc9a85d7d787
harryx <a@b>
2016-08-10 13:26:43 -0800

    [ORCL-155]copy OCI libraries for bamboo test.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 186927]

--- Bamboo/Scripts/NV_modules/DriverOdbcFunctionalTestExtra.pm
+++ Bamboo/Scripts/NV_modules/DriverOdbcFunctionalTestExtra.pm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f722bda41cb0397cc51211042ebeb65e3cd9976ad441b514c00316fcaf39fbe5
-size 1126
+oid sha256:fc7e241b37eaa7e5735903dbf27ec694142f6d7ee9b18716cb106e8a620ff860
+size 1929
