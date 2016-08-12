e6497e7a25128372270d87d09e087da63cd6bbb5
harryx <a@b>
2016-08-11 18:44:03 -0800

    [ORCL-159]Bug fix: Empty data might be identified as NULL.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 187219]

--- Source/DataEngine/OCINonArraySimpleResultSet.cpp
+++ Source/DataEngine/OCINonArraySimpleResultSet.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be379f79a6cf16396787f1503579011ea4d9b24b9c63225c64c91e857165dd51
-size 12215
+oid sha256:2d5efca253390c9dedbbe918a9c81b31a2a796d29ab0d73827d65a17c557d1f9
+size 12265
--- Source/DataEngine/OCISimpleResultSet.cpp
+++ Source/DataEngine/OCISimpleResultSet.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c65bbf877c4d5b2c06ae017d3a5bc0c380605f02e246abdc04056747db991caf
-size 14266
+oid sha256:faa921b411029f65a4372d4b41c2eaa1abd5b169637ad47e0ff153ae0ab55280
+size 14316
