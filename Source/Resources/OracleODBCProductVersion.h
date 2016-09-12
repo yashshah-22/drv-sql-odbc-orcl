a177833af52890c63d32a90a890d048c1e3cbec0
garyq <a@b>
2016-09-12 12:19:28 -0800

    [ORCL-179] Change branding to Simba
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 192107]

--- Source/Resources/OracleODBCProductVersion.h
+++ Source/Resources/OracleODBCProductVersion.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7045b8e703d4533cdd3bca9d9e63c801e08d8b156bf4f11c56123d539a3e1eba
-size 927
+oid sha256:6ae02c2c2aa3e5b4c999acb05793e8c060b14041ffd00e1c3d5e2bfb9f8cbdbc
+size 810
