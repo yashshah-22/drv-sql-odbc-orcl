4cc90042711d1b7cff615593cafc459e9cd108b0
garyq <a@b>
2016-09-16 13:16:54 -0800

    [ORCL-179] Revert Product Information to have all the necessary keys
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 193271]

--- Source/Resources/OracleODBCProductVersion.h
+++ Source/Resources/OracleODBCProductVersion.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ae02c2c2aa3e5b4c999acb05793e8c060b14041ffd00e1c3d5e2bfb9f8cbdbc
-size 810
+oid sha256:86b39bb97a4576fcab690afc918f2058bbaa020e8d11b19b715bbdf8fa6942fd
+size 974
