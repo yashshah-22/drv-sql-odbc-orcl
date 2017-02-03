63933d72629b0698654548d32764bf51eb9830eb
brianw <a@b>
2017-02-03 15:05:14 -0800

    Encoding fix for Oracle ODBC Functional tests for UNIXODBC. UTF32 -> UTF16
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 216783]

--- Bamboo/Scripts/NV_modules/MakeLinuxIniFiles.pm
+++ Bamboo/Scripts/NV_modules/MakeLinuxIniFiles.pm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc36d406f727660be68126bd13614e9643503bfab889760e29d27e44397608b
-size 5771
+oid sha256:df3d3e7343b8f4006d25f7be8b966aad717ce6fc3dddcfe712146ccb207badb5
+size 5773
