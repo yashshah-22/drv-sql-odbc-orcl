e59269c95be3959613406457cc117358059b9499
brianw <a@b>
2017-02-08 15:24:56 -0800

    [ORCL] Revised name for binary DLL, remove Simba name from output driver bin file
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 217535]

--- Bamboo/Scripts/NV_modules/SetWindowsRegistryForTest.pm
+++ Bamboo/Scripts/NV_modules/SetWindowsRegistryForTest.pm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eabf40f929986725bfac07c8372e07b6b904294e1c26aaa649bf96bebf0e8b90
-size 4552
+oid sha256:6d778792de94dd862ccd89d186b55610aa178dec632d9fe5e3925db9715f5aca
+size 4508
