9b7d8adb797856f49e50fd8f29ace4fe2369c6cb
harryx <a@b>
2017-04-24 15:50:08 -0800

    [ORCL-241]Fix crash issue on Windows 32bit when try to test connection with invalid user name in DSN configuration Dialog.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.1/Product/": change = 231722]

--- Source/Setup/ConfigDSN.cpp
+++ Source/Setup/ConfigDSN.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2172e016682fd369637ecf1112078b9a14db26ca4733a558712e2b0abaf0ecb1
-size 10177
+oid sha256:7bad8cea16877bf65498351e00e556be07897d453058f6f5a868cda498bfec41
+size 11064
