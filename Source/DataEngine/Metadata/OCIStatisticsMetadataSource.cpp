ac6fc8b6442ebb767f0aca22ce96a77abba811fd
harryx <a@b>
2017-12-11 11:15:57 -0800

    [ORCL-279]MS Access got error because SQLStatistics() return unexpected data.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.2/Product/": change = 270669]

--- Source/DataEngine/Metadata/OCIStatisticsMetadataSource.cpp
+++ Source/DataEngine/Metadata/OCIStatisticsMetadataSource.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea820ca36bead33409d34ba912d9682469a5f1d046ac09dcaaf39e0199bffe9b
-size 7021
+oid sha256:fd336c10ce90c9526751d1dc69c8b96a2d2e7692f2c1365c2660d87187b1d92b
+size 7200
--- Source/DataEngine/OCIDataEngineUtilities.h
+++ Source/DataEngine/OCIDataEngineUtilities.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c31d4f61b29c288b1fb080d74fdca936f43b2646b127f08c2d56de8e81b462e
-size 9420
+oid sha256:d38a8405255f5a0af8ed486c09900ebf0e5314fa74baf041454e5269def18242
+size 9355
