ac11c8348bc028c65f8611e32e5ab90e27b531b6
bchandrasekar <a@b>
2018-01-31 13:29:39 -0800

    OCI Depricated packages removal
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.2/Product/": change = 278871]

--- Source/Core/OCIStatement.cpp
+++ Source/Core/OCIStatement.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f9f676f8362fb51141f06c93736b9ec1d4fc47fc623ebf17fd57147dad15014
-size 3234
+oid sha256:a0e36adc539f7906615a6efa02816d88b3510b6f2961a44619b1d9441f3ad13c
+size 3128
--- Source/DataEngine/Metadata/OCIMetadataSource.cpp
+++ Source/DataEngine/Metadata/OCIMetadataSource.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2351d73f7417b110552bc8b536d289e8aad1066cf4bb9d1b6d92ccbd82d39e6e
-size 4648
+oid sha256:75ded072f2667dceae2e94e90fa72be642c0a69a17f2d27c4939ce562934e389
+size 4978
