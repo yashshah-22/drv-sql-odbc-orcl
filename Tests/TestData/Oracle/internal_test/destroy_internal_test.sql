ca6472921cd45396962151377c58bc60c0332396
bchandrasekar <a@b>
2018-02-08 23:50:44 -0800

    DML Touchstone Tests
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.2/Product/": change = 281572]

--- Tests/TestData/Oracle/internal_test/destroy_internal_test.sql
+++ Tests/TestData/Oracle/internal_test/destroy_internal_test.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7d0746c476358de09fe9cc29e65eb2ba5b00ad3ca01a2e561e5080dc18edb0
-size 2203
+oid sha256:4b07fd0bd26dc5ee79e87456bb04719c831ae6e7b1a7e26699a4bedba399e198
+size 2311
--- Tests/TestData/Oracle/internal_test/load_internal_test.sql
+++ Tests/TestData/Oracle/internal_test/load_internal_test.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bbee7ac83a33682187536a383533c2659b6ee864c0a735a6de93e1c40b9e850
-size 210174
+oid sha256:7b6a2b449b7b574ebd2312297eae51d373d518303eb827f38078a5f9a439b4ac
+size 213006
