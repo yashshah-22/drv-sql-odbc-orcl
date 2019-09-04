5aeeafceb167b63ff62f2a34aeb00ca944c6c488
KReddy <a@b>
2019-09-04 00:14:54 -0800

    Fix Disconnect Issue
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.3/Product/": change = 451856]

--- Source/Core/OCIConnection.cpp
+++ Source/Core/OCIConnection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fd5f7a7d3940e612a70a04c7dea7881e8fc62837b19c5055f15bd8621d1cd6e
-size 53733
+oid sha256:2c7abd6e71e0c20805dc6a1344a8c5b5f38a1c7de4c7d4ed3a169105cef8a618
+size 53612
