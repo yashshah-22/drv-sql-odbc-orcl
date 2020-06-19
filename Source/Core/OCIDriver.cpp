a74c1a0f1d863877daf77a1b797ec8acdc3178a9
KReddy <a@b>
2020-06-18 20:24:08 -0800

    [ORCL-625] Add custom Double type
    Since setting the userdatatype for Double type has no impact and is not used anywhere, it will not be set.
    And since Double type is no longer considered as a custom datatype, we will no longer need to add this to the plugin
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 588894]

--- Source/Core/OCIDriver.cpp
+++ Source/Core/OCIDriver.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74e8d2f64844528c80be341ffcfdd6603b4145ef50e3c154011328b91441d7f8
-size 9171
+oid sha256:09ad16759bd53bb9ffcd656ff35dc48548429d237d39ac5a01fcded26973fe92
+size 9138
--- Source/DataEngine/OCIDataEngine.cpp
+++ Source/DataEngine/OCIDataEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cda624b500994963b2da03acf077ee51a2ec5a55698b17b9f034ac951566175d
-size 58948
+oid sha256:ad092cf76e8cdf27a422dd6e247d40dd3503ef4e984d36eb758cab4d4bfcfe7f
+size 58894
