74e96aa200a217379e9eba56286601d3fa0f2804
KReddy <a@b>
2020-06-23 21:48:06 -0800

    [ORCL-628] Remove custom type for binary float
    Having a userdataype (!=0) will be considered as custom datatype by SEN and will require a custom datatype to be defeind in the plugin and since the userdatatype is being set and not used anywhere we can go ahead and not set thus keep its default value (=0).
    
    Refer [ORCL-625] for more details
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 592239]

--- Source/DataEngine/OCIDataEngine.cpp
+++ Source/DataEngine/OCIDataEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad092cf76e8cdf27a422dd6e247d40dd3503ef4e984d36eb758cab4d4bfcfe7f
-size 58894
+oid sha256:cbe750ae1b5c716fcc84bdd3017dd129382e85a9f5df4b16bc3aea13d181c6d6
+size 58841
