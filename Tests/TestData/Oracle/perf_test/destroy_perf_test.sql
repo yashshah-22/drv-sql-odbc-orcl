4c2f72ebf1a4a15b3aa946ceecbe8fb106463e51
KReddy <a@b>
2020-07-01 05:49:00 -0800

    [ORCL-637] Create perf_test script and insert data into local DS
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 596986]

---
+++ Tests/TestData/Oracle/perf_test/destroy_perf_test.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ccdc5c217dc519331e9e0a771eea7f928ffa25090722f35ddce0e21cf83f6b5b
+size 70
---
+++ Tests/TestData/Oracle/perf_test/load_perf_test.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d3550b3023e8537798c81b742f3f7a9092c0bf74782afdd6b38bc207a6ec59e
+size 29418
