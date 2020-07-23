d6bbfda982cafd631c24d03d077ad6926e589068
KReddy <a@b>
2020-07-22 22:19:04 -0800

    [ORCL-647] Data conversion algorithm not supported
    Since we dont have any custom C->SQL converters we need to use default C->SQL converters
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 614332]

--- Source/OracleODBCClientPlugin/OracleODBCPlugin.cpp
+++ Source/OracleODBCClientPlugin/OracleODBCPlugin.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc6543173b815d39cfee4828d8c3113d7f52fb680079f5374d80f38ce34daf81
+oid sha256:176fbde4dc5d06214af36154799bb5ce7ad4af3b9a02b5b1afc7774dd5b5d402
 size 22301
