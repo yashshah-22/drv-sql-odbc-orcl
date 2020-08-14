1c23b692b65f682807ce54ba1e73c919fdffe503
KReddy <a@b>
2020-08-13 22:35:17 -0800

    [ORCL-656] Issues in writing  6 decimal values  (Some Specific Pattern)
    The calculation for triming the trailing 0s after the decimal point is not considering the whole numbers.
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 628056]

--- Source/CustomDataTypes/Converters/OCISTCNumericToCharCvt.cpp
+++ Source/CustomDataTypes/Converters/OCISTCNumericToCharCvt.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b69d440ba4566f2e39bc6a128ac2e4ac0daaaa9c2be5f1b14d8dee7cd334006
-size 16026
+oid sha256:0d0eeced3225a48e548e004d27f584721ae9967ff7fd3351573ba089f43b6c3a
+size 16047
