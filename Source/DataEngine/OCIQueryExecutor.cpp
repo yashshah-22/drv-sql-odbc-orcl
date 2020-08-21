ee4038f28581cfe4f52b296c3737782eb24f93b1
KReddy <a@b>
2020-08-21 04:20:06 -0800

    [ORCL-654] Internal error on trying to write to write SpatialInfo
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 632874]

--- Source/DataEngine/OCIQueryExecutor.cpp
+++ Source/DataEngine/OCIQueryExecutor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb9eb47aba10ca4db47472ac765192b7f0034a7b13595d79091b49a02a165ce9
-size 47937
+oid sha256:bdd4f544ae2c3e1aaf02baaf9350eea87760ce7837e0f2ad3d59997ebc9cd3f6
+size 50226
--- Source/OracleOCI.h
+++ Source/OracleOCI.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35d82a77bf85100bab96cd57ba9081f437b040805c6312adbc8b3dd2ba78725f
-size 32744
+oid sha256:70f857373952af9aae90d930fad6283057a0c32ccf0ff8fd0205cc5e671723de
+size 32795
