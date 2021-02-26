7b481170aedc35de9ef58096bb8cd9008df480a5
KReddy <a@b>
2021-02-26 08:13:29 -0800

    [ORCL-696] ERROR [HY000] [Qlik][OracleOCI] (3000) Oracle Caller Interface: ORA-24347: Warning of a NULL column in an aggregate function
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 708006]

--- Source/DataEngine/OCIFetchThread.cpp
+++ Source/DataEngine/OCIFetchThread.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ba8537abb8e3c64218e88a1dad52a3b86b44a69a0c7b99614c95da8f15e4574
-size 10438
+oid sha256:de3206b00e69fe5e75c8c645ce1875d8695968e2bcb21a49b0994b416fd05263
+size 10446
