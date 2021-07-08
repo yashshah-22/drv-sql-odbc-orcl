a78f118f4c8a5eac4023910e755d2f97e295234a
KReddy <a@b>
2021-07-07 21:44:43 -0800

    [ORCL-743] Fix Drmemory failures for gateway
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/1.4/Product/": change = 767072]

--- Source/OracleODBCClientPlugin/PluginOCINClobType.cpp
+++ Source/OracleODBCClientPlugin/PluginOCINClobType.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c62ef9a611a13e4940f1814868242c1fdbd1bbc536e49a66da2cde1f23bd3c36
-size 7218
+oid sha256:7c1c28fb90438f046179018949e7f24e11c0d9b9fd241cd29bbcc587453e3eaf
+size 7201
