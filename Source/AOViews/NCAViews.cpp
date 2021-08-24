fa904a8b7b195e4fb61b74d536e352df97058058
Matthew Wozniczka <matthew.wozniczka@insightsoftware.com>
2021-08-24 14:13:56 -0800

    [AOViews] Define LOG_NAMESPACE & use it in logging.
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 786667]

--- Source/AOViews/NCAViews.cpp
+++ Source/AOViews/NCAViews.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f08e8edd53c9b6065587120ea1e45909e4ad41ead439c87957160b32bdaba2e2
-size 82324
+oid sha256:757301c969bb3621f03027d636c4c56806fbc4cd06eb041460ae0ee285e368b1
+size 82294
--- Source/AOViews/NCAViews.h
+++ Source/AOViews/NCAViews.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:698ffa369217d373aaa2957904dc5a2b6ca601ce3846541faf40af036e65b3a0
-size 422
+oid sha256:d9c617ad2a0f1ddc6c090e743e778c46465f9c38306c3fc7214a10c1fdcbdbec
+size 463
