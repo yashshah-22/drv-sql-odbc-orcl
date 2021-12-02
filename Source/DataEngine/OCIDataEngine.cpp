a6a507882e490d59d1bf6b3cdac686be7aae9dc3
KReddy <a@b>
2021-12-01 21:20:40 -0800

    [ORCL-748] Oracle Merge Statement Not Supported
    Copying changes from 1.4 to 2.0 branch
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 812919]

--- Source/DataEngine/OCIDataEngine.cpp
+++ Source/DataEngine/OCIDataEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1360709701c699dc107e4cfee7e4d3e146bcf9905f9d7b59a177eb047165465d
-size 62222
+oid sha256:fd6746a5ce21bdc44904dfc9cb9040ed287887c6fb07c213afe3d868e6aa31dc
+size 66867
