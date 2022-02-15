f7eb497a1520df585adec9c489028f6f3c17cacc
KReddy <a@b>
2022-02-15 00:19:52 -0800

    [ORCL-773] Oracle Merge statements with bind variables only support the first row of bind variables
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 829236]

--- Source/DataEngine/OCIQueryExecutor.cpp
+++ Source/DataEngine/OCIQueryExecutor.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa26b751253cbdee3c8fcc340430e01b1b8a3c470b78b539276f9488341e089e
-size 53499
+oid sha256:64ba74ddfbe3cef9e78ce743d1a307122b92615a46ecd5312856f296c05dc636
+size 54744
