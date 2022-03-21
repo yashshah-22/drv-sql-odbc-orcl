68dd9aca1af5db20c53722dd0d54ad675f74dbf6
KReddy <a@b>
2022-03-21 11:18:19 -0800

    Copy Angles changes from Driver Shared to Oracle Depot
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 837945]

--- Source/AOViews/NCAViews.cpp
+++ Source/AOViews/NCAViews.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:757301c969bb3621f03027d636c4c56806fbc4cd06eb041460ae0ee285e368b1
-size 82294
+oid sha256:1c8888bc842c8c4c50fa011a7a16af37c361da4c9dddf811049ede0745cf6218
+size 81046
