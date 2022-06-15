178b54c7cf7e15bdd13d5b2a72f1aced8a3f3e41
KReddy <a@b>
2022-06-15 02:57:18 -0800

    [ORCL-757] Add Touchstone Tests for Angles for Oracle
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 857296]

--- Tests/GatewayAdapter/ACPConstants.java
+++ Tests/GatewayAdapter/ACPConstants.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ff41c85eb413a16eaf78ffe1ab162bb621a65354935d94ba49f1a621db9b067
-size 2509
+oid sha256:e9045196d80dbb750c28e49840f3c8fe134d9d16bc2fbcbd13986cc8dffac874
+size 2784
