ea2e2799b94f6fd034e391ad5ddceb4c7928c7a2
KReddy <a@b>
2023-01-12 02:00:35 -0800

    [ORCL-826] Remove Decryption of PWDE for Posix
    We don't manually add PWDE, it added by GUI and since Posix platforms don't have GUI, we don't use PWDE
    
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 900083]

--- Source/Core/OCIConnection.cpp
+++ Source/Core/OCIConnection.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05157acb2eaa6b5135e775c2e630b9130cd4d26def736e091bb6700ef6f27e22
-size 57520
+oid sha256:571382636b759a8a3ee44263d5b3cf91fada7b7eab30ff82bf905f4c1959c4fd
+size 57431
