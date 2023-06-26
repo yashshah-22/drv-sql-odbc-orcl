ed63369bcdcb5f0c70c128cdaf66472b46934f14
Pratik Pampattiwar <pratik.pampattiwar@insightsoftware.com>
2023-06-25 22:55:54 -0800

    [ORCL-844] Change in parsing of the sql query to pick table name correctly
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 935345]

--- Source/DataEngine/OCIDataEngine.cpp
+++ Source/DataEngine/OCIDataEngine.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35a6cedf6e7b55505f4d62e6133d0cbab12e2985247f37afa319f0ecd3baf9f4
-size 74049
+oid sha256:7544f48e09627b3edabadfca10facaf73684c4b429de8d5c018459735b1935c9
+size 74661
