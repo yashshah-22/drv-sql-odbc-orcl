104195f01249ff8214d291d73b84848c2cc5efd2
Pratik Pampattiwar <pratik.pampattiwar@insightsoftware.com>
2023-09-27 23:13:46 -0800

    [ORCL-850] Bug Fixes for Sonar scan
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 963697]

--- Source/Core/IAutoCommitChangedListener.h
+++ Source/Core/IAutoCommitChangedListener.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84449dea1b6533d80f1ee55d03eacb85848b660542939018e14d159f78934e1f
-size 1412
+oid sha256:462db612ceac9d718bbcf6d9353828c6ea4298b13de19750bc984cf677676817
+size 1550
