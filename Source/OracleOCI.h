72108b3649a0f0fad99f2e235f821b9a5767e113
Shivamkumar Dabas <sdabas@magnitude.com>
2024-02-16 03:01:45 -0800

    [ORCL-855] Oracle SSL Cert change
    [git-p4: depot-paths = "//Drivers/OracleODBC/Maintenance/2.0/Product/": change = 1035718]

--- Source/OracleOCI.h
+++ Source/OracleOCI.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2fa31cfd4a05f47bcf56210daf25dba61524f147b3b9968b89cc96495c3adc0
-size 27711
+oid sha256:119319ecdc507cb58fff295162c29c0cad1144bd13b4c56cf90691a7279541da
+size 28007
