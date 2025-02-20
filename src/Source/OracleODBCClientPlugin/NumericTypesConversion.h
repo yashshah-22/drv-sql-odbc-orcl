b8f8bef83373677135116c0eba06a91bcc97ce27
priya20s <priya.singh@insightsoftware.com>
2025-02-20 16:08:52 +0530

    AnglesforOracle changes

---
+++ src/Source/OracleODBCClientPlugin/NumericTypesConversion.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:498f644c5046135bea7cd8b06c36c51b8a5929387f47360633cb1377749ff6b1
+size 53594
---
+++ src/Source/OracleODBCClientPlugin/UnicodeStringTypesConversion.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2d5aaaa8a37969026f1aee4f2b04ed6944d2a977f69c79c156c0d2a9532c59a
+size 5552
